c2dd6d9cb5def03c7c58d0ee894495e1eb24b42b
anton.kozak <akozak@lenovo.com>
2025-10-15 13:36:53 +0200

    huge amount of changes just for linting check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ci-cd-practice';
-  private myBadVariable = 'hello';
+  private myBadVariable = 'test';
+
+  /**
+   *
+   */
+  constructor() {
+    this.someunusedMethod();
+  }
+
+  someunusedMethod(): void {
+    // console.log(log);
+  }
 }
